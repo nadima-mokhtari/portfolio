8b61b6afcd059bec8fd34edd1cdd2374fc1cc279
nadima-mokhtari <nadima.mokhtari.dwwm48@gmail.com>
2025-01-22 09:11:28 +0100

    update

--- index.js
+++ index.js
@@ -1,15 +1,33 @@
-document.getElementById ('formContact').addEventListener ('submit', function (event) {
-    const message = document.getElementById ('message').value;
-    const nom = document.getElementById ('nom').value;
-    const email = document.getElementById ('email').value;
+
+document.getElementById('formContact').addEventListener('submit', function(event) {
+    const message = document.getElementById('message').value;
+    const nom = document.getElementById('nom').value;
+    const email = document.getElementById('email').value;
+
+
     if (message === '') {
-        alert ('Le message sont obligatoires.');
-        event.preventDefault ();
+        alert('Le message est obligatoire.');
+        event.preventDefault();
         return;
     }
+
+    console.log ('message')
+
     if (nom === '' || email === '') {
-        alert ('Le nom et l\'email sont obligatoires.');
-        event.preventDefault ();
+        alert('Le nom et l\'email sont obligatoires.');
+        event.preventDefault();
         return;
     }
-})
+
+    const emailPattern = /^[^@]+@[^@]+\.[^@]+$/;
+    if (!emailPattern.test(email)) {
+        alert('Veuillez entrer un email valide.');
+        event.preventDefault();
+        return;
+    }
+
+    
+});
+
+
+
